8dad3e9792b08ee3a8a2383e1f2d31dda9a9fadb
corkesw <home@stephencorke.com>
2021-11-01 15:49:07 +0000

    add user auth sandbox for debugging

--- App.js
+++ App.js
@@ -3,16 +3,19 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Header from './Header';
 import Login from './Login';
+import Routes from './navigation';
+
+import { registerRootComponent } from 'expo';
 
 export default function App() {
   return (
     <>
-    <Header />
-    <Login />
-    <Nav />
+    <Routes />
     </>
   );
 }
+
+registerRootComponent(App);
 
 const styles = StyleSheet.create({
   container: {
---
+++ config/Firebase.js
@@ -0,0 +1,27 @@
+import {initializeApp, getApp, getApps} from 'firebase/app';
+import 'firebase/auth';
+import Constants from 'expo-constants';
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: Constants.manifest.extra.apiKey,
+  authDomain: Constants.manifest.extra.authDomain,
+  projectId: Constants.manifest.extra.projectId,
+  storageBucket: Constants.manifest.extra.storageBucket,
+  messagingSenderId: Constants.manifest.extra.messagingSenderId,
+  appId: Constants.manifest.extra.appId
+};
+console.log(firebaseConfig)
+
+let Firebase;
+Firebase = initializeApp(firebaseConfig)
+
+// if (getApps().length === 0) {
+//     Firebase = initializeApp(firebaseConfig)
+// } else { 
+//     getApp()
+// }
+
+console.log(Firebase.auth, '<<< auth')
+console.log(Firebase.default, '<<<default')
+export default Firebase;
