40371657787b83191cda85163d3ef5a77d34b900
Mohamed Sobh <eng.m_sobhe@yahoo.com>
2021-11-02 14:16:50 +0000

    add drawer to home stack

--- navigation/HomeStack.js
+++ navigation/HomeStack.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import HomeScreen from "../screens/HomeScreen";
 
-const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
 
 export default function HomeStack() {
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Home" component={HomeScreen} />
-    </Stack.Navigator>
+    <Drawer.Navigator screenOptions={{ headerShown: false }}>
+      <Drawer.Screen name="Home" component={HomeScreen} />
+    </Drawer.Navigator>
   );
 }
