aef9b23dfffd0ba9ff0f9be4a193eab84ad8a2bb
Mohamed Sobh <eng.m_sobhe@yahoo.com>
2021-11-03 18:40:58 +0000

    add some styles for walker's card

--- screens/ListWalkers.js
+++ screens/ListWalkers.js
@@ -35,17 +35,19 @@ const ListWalkers = () => {
     );
   console.log(walkers);
   return (
-    <View>
+    <View style={styles.container}>
       <Text style={styles.title}>All Walkers in your area</Text>
-      {walkers.map((walker) => {
-        return (
-          <View>
-            <Text>First name: {walker.firstname}</Text>
-            <Text>Las name: {walker.lastname}</Text>
-            <Text>Post code: {walker.postcode}</Text>
-          </View>
-        );
-      })}
+      <View style={styles.card}>
+        {walkers.map((walker) => {
+          return (
+            <View>
+              <Text>First name: {walker.firstname}</Text>
+              <Text>Las name: {walker.lastname}</Text>
+              <Text>Post code: {walker.postcode}</Text>
+            </View>
+          );
+        })}
+      </View>
     </View>
   );
 };
@@ -53,7 +55,23 @@ const ListWalkers = () => {
 export default ListWalkers;
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+  },
   title: {
     fontSize: 30,
+  },
+  card: {
+    borderColor: "black",
+    borderStyle: "solid",
+    backgroundColor: "#FFA45E",
+    borderWidth: 4,
+    borderRadius: 6,
+    marginTop: 16,
+    paddingVertical: 8,
+    textAlign: "center",
+    fontSize: 30,
+    fontWeight: "bold",
   },
 });
